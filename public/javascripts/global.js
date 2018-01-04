0f18d0039d84104c609924e98e174ece7747bf11
EduardoBBarbosa <brandao.barbosa@outlook.com>
2018-01-04 00:01:36 -0200

    show user info ok

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -29,7 +29,7 @@ function populateTable() {
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function () {
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
             tableContent += '<td>' + this.email + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
@@ -46,7 +46,7 @@ function showUserInfo(event) {
 
     var thisUserName = $(this).attr('rel');
 
-    var arrayPosition = userListData.map(function (arrayItem) { return arrayItem.username; }).indexOf(this.thisUserName);
+    var arrayPosition = userListData.map(function (arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
 
     var thisUserObject = userListData[arrayPosition];
 
